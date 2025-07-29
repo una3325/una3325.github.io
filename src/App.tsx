7389a5eb7b95ad6169fd7c67e6a80b6d30b40a0f
una3325 <a33256448@gmail.com>
2025-07-29 17:51:01 +0900

    chore: 초기 환경 설정

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <section>
+        {/* Home */}
+        <h1>Una's Portfolio</h1>
+        <p>Frontend Developer</p>
+      </section>
+      <section> Profile </section>
+      <section> Skills </section>
+      <section> Projects </section>
+      <section> Footer </section>
+    </div>
+  );
+}
+
+export default App;
